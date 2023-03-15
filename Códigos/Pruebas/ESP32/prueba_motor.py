3bc93df007792c559377b924a00783824024dfbf
Juan Manuel Hincapié <119358374+juanma010901@users.noreply.github.com>
2023-03-15 11:20:44 -0500

    PWM correct test

--- Códigos/Pruebas/ESP32/prueba_motor.py
+++ Códigos/Pruebas/ESP32/prueba_motor.py
@@ -3,24 +3,16 @@ import board
 import pwmio
 from adafruit_motor import servo
 
-# Configurar el pin 27 como una salida PWM
-pwm = pwmio.PWMOut(board.D5, duty_cycle=2 ** 15, frequency=50)
+# Configurar el pin 32 como una salida PWM
+pwm = pwmio.PWMOut(board.D32, duty_cycle=2 ** 15, frequency=50)
 
 # Crear un objeto servo
 servo = servo.Servo(pwm, min_pulse=500, max_pulse=2500)
 
-# Mover el servo a 0 grados
-servo.angle = 0
-time.sleep(1)
+while True:
+    
+    # Mover el servo
+    grados = int(input("Ingrese el ángulo: "))
+    servo.angle = grados
 
-# Mover el servo a 90 grados
-servo.angle = 90
-time.sleep(1)
-
-# Mover el servo a 180 grados
-servo.angle = 180
-time.sleep(1)
-
-# Detener el servo en su posición actual
-servo.throttle = None
-
+#CODIGO FUNCIONANDO CORRECTAMENTE
