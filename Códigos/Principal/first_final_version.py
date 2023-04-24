ec636566b64750826483e9da77ef413997420c89
Juan Manuel <juanmahc0109@gmail.com>
2023-04-24 15:34:29 -0500

    Static IP try

--- Códigos/Principal/first_final_version.py
+++ Códigos/Principal/first_final_version.py
@@ -4,6 +4,7 @@ import analogio
 import board
 import busio
 import digitalio
+import ipaddress
 import json
 import math
 import secrets
@@ -23,12 +24,18 @@ password = secrets.secrets["password"]
 #aio_username = secrets.secrets["aio_username"]
 #aio_key = secrets.secrets["aio_key"]
 
+#EndPoints API
 getPuntos = "https://masterusers.azurewebsites.net/api/GetPuntos"
 getModo = "https://masterusers.azurewebsites.net/api/GetModoActual"
 updateModo = "https://masterusers.azurewebsites.net/api/ActualizarModo"
 
-#wifi.radio.ipv4_address = "192.168.1.100"
+#Set static IP address
+# ipv4 =  ipaddress.IPv4Address("192.168.1.3")
+# netmask =  ipaddress.IPv4Address("255.255.255.0")
+# gateway =  ipaddress.IPv4Address("192.168.1.1")
+# wifi.radio.set_ipv4_address(ipv4=ipv4,netmask=netmask,gateway=gateway)
 wifi.radio.connect(ssid, password)
+
 print("Connected to %s!" % ssid)
 print("My IP address is", wifi.radio.ipv4_address)
 
